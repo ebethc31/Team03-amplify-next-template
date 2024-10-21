958209fef372cb16b01d90f5ba0a8e9c39830db2
Liz <eachand@clemson.edu>
2024-10-21 19:58:45 -0400

    added about page link to main page

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
-
+//Amplify.configure(outputs);
+/*
 const client = generateClient<Schema>();
 
 export default function App() {
@@ -46,6 +46,33 @@ export default function App() {
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
+      </div>
+    </main>
+  );
+}
+*/
+
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        {/* <AcmeLogo /> */}
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Welcome to CPSC 4910 Project.</strong> This is the default homepage for our website!
+          </p>
+          <Link
+            href="/about"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>About</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+        </div>
       </div>
     </main>
   );
